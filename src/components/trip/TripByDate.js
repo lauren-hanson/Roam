b5ba4b150195642d04b7ef38eb3f4a209e802197
Lauren Hanson <lohanson33@gmail.com>
2023-03-18 18:23:21 -0500

    filter between upcoming/all

--- src/components/trip/TripByDate.js
+++ src/components/trip/TripByDate.js
@@ -1,17 +1,8 @@
-import { useEffect, useState } from "react"
-import { getUpcomingTrips } from "../../managers/TripManager"
 
-
-export const TripByDate = ({setFilteredTrips, tripId, myTrips}) => {
-
-    const [upcomingTrips, setUpcomingTrips] = useState([])
-
-    useEffect(() => {
-        getUpcomingTrips(tripId).then((tripData) => setUpcomingTrips(tripData))
-    }, [, tripId])
+export const TripByDate = ({setFilteredTrips, upcomingTrips, trips}) => {
 
     return (<>
         <button className="button is-small is-rounded" onClick={() => setFilteredTrips(upcomingTrips)}>View Upcoming</button>
-        <button className="button is-small is-rounded" onClick={() => setFilteredTrips(myTrips)}>View All</button>
+        <button className="button is-small is-rounded" onClick={() => setFilteredTrips(trips)}>View All</button>
     </>)
 }
--- src/components/trip/TripList.js
+++ src/components/trip/TripList.js
@@ -8,9 +8,9 @@ import "./Trip.css"
 export const TripList = ({ token }) => {
 
     const [trips, setMyTrips] = useState([])
-    const [myTrips, setFilteredTrips] = useState([])
+    const [myTrips, setFilteredTrips] = useState(trips)
     const [upcomingTrips, setUpcomingTrips] = useState([])
-    const [pastTrips, setpastTrips] = useState([])
+    const [pastTrips, setpastTrips] = useState(false)
 
     const [searchTerms, setSearchTerms] = useState("Search Posts By Title")
 
@@ -28,30 +28,31 @@ export const TripList = ({ token }) => {
 
     useEffect(() => {
         getUpcomingTrips(tripId).then((tripData) => setUpcomingTrips(tripData))
+        
     }, [])
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        getSearchedTrips(`${searchTerms}`).then((data) => setFilteredTrips(data))
-        setSearchTerms("Search Trips By Title")
-        document.getElementById("search").value = ""
-    }
+    // const handleSubmit = (e) => {
+    //     e.preventDefault()
+    //     getSearchedTrips(`${searchTerms}`).then((data) => setFilteredTrips(data))
+    //     setSearchTerms("Search Trips By Title")
+    //     document.getElementById("search").value = ""
+    // }
 
-    const handleKeypress = (e) => {
-        //it triggers by pressing the enter key
-        if (e.keyCode === 13) {
-            handleSubmit()
-        }
-    }
+    // const handleKeypress = (e) => {
+    //     //it triggers by pressing the enter key
+    //     if (e.keyCode === 13) {
+    //         handleSubmit()
+    //     }
+    // }
 
 
     return <>
         <section className="trip__array">
             <div><button onClick={() => navigate(`/trips/newtrip`)}>+</button>New Trip</div>
             <h2>My Trips</h2>
-            <SearchTrips searchTerms={searchTerms} setSearchTerms={setSearchTerms} handleSubmit={handleSubmit} setFilteredTrips={setFilteredTrips}
-                handleKeypress={handleKeypress} trips={trips} myTrips={myTrips} />
-            <TripByDate setFilteredTrips={setFilteredTrips} myTrips={myTrips}/>
+            {/* <SearchTrips searchTerms={searchTerms} setSearchTerms={setSearchTerms} handleSubmit={handleSubmit} setFilteredTrips={setFilteredTrips}
+                handleKeypress={handleKeypress} trips={trips} myTrips={myTrips} /> */}
+            <TripByDate setFilteredTrips={setFilteredTrips} tripId={tripId} upcomingTrips={upcomingTrips} trips={trips}/>
             {
                 myTrips.map((trip) => (
                     <>
