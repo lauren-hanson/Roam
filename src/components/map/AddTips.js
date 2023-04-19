f4236c62a2c9bf3a97df589a566ee65c5b14301a
Lauren Hanson <lohanson33@gmail.com>
2023-04-19 12:21:48 -0600

    tip textarea rendering and grabbing correct destination

--- src/components/map/AddTips.js
+++ src/components/map/AddTips.js
@@ -1,10 +1,28 @@
-export const AddTips = ({ updateDestination, favDestinations }) => {
+export const AddTips = ({ updateDestination, favDestinations, id }) => {
+
+    const handleAddTips = () => {
+
+        const data = {
+            // destination_id: selectedDestination,
+            ...favDestinations,
+            tips: ""
+        }
+
+        updateDestination(id, data)
+            // setRefresh(!refresh)
+            .then(() => {
+                window.location.reload()
+            })
+    }
 
     return (
         <>
             <textarea
                 placeholder="Any tips?"
             />
+            <button
+                onClick={handleAddTips}
+                class='button is-small'>+</button>
         </>
     )
-}
+}   
