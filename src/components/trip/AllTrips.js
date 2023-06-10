87aad75a1ca0834471d479e0d251e77c5fc7424a
Lauren Hanson <lohanson33@gmail.com>
2023-06-09 21:11:19 -0700

    search bar working

--- src/components/trip/AllTrips.js
+++ src/components/trip/AllTrips.js
@@ -4,14 +4,32 @@ import { TripByTag } from "./TripByTag"
 import { getPublicTrips, getSearchedTrips } from "../../managers/TripManager"
 import { HumanDate } from "../utils/HumanDate";
 import "./Trip.css"
-import { TripSearch } from "./TripSearch"
+// import { TripSearch } from "./TripSearch"
 
 export const AllTrips = ({ token }) => {
     const [trips, setTrips] = useState([])
     const [tagChoice, setSelectedTripByTag] = useState(0)
     const [searchTerms, setSearchTerms] = useState("Search Trips by Keyword")
+    const [filteredTrips, setFilteredTrips] = useState([])
 
 
+    useEffect(() => {
+        setFilteredTrips(trips)
+    }, [])
+
+    const handleKeypress = (e) => {
+        //it triggers by pressing the enter key
+        if (e.keyCode === 13) {
+            handleSubmit()
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        getSearchedTrips(`${searchTerms}`).then((data) => setFilteredTrips(data))
+        setSearchTerms("Search Trips by Keyword")
+        document.getElementById("search").value = ""
+    }
 
     const navigate = useNavigate()
 
@@ -19,13 +37,27 @@ export const AllTrips = ({ token }) => {
         getPublicTrips().then((tripData) => setTrips(tripData))
     }, [])
 
-    const filteredTrips = tagChoice
-        ? trips.filter((trip) => trip.tag.some((tag) => tag.id === tagChoice))
-        : trips
 
     return (
         <>
-            <TripSearch searchTerms={searchTerms} setSearchTerms={setSearchTerms} getSearchedTrips={getSearchedTrips} trips={trips} />
+            <section className="posts__buttons">
+                <section className="posts__filters">
+                    <form>
+                        <input
+                            type="textfield"
+                            placeholder={searchTerms}
+                            id="search"
+                            onChange={(e) => setSearchTerms(e.target.value)}
+                            onKeyUp={handleKeypress}
+                        ></input>
+                        <button className="button is-small" type="submit" onClick={handleSubmit}>
+                            Go
+                        </button>
+                        <button className="button is-small" onClick={() => setFilteredTrips(trips)}>View All</button>
+                    </form>
+                </section>
+            </section>
+            {/* <TripSearch searchTerms={searchTerms} setSearchTerms={setSearchTerms} getSearchedTrips={getSearchedTrips} trips={trips} /> */}
             <TripByTag setSelectedTripByTag={setSelectedTripByTag} tagChoice={tagChoice} trips={trips} />
             <div key={`trips--${trips.id}`}>
                 <div className="allTripList">
